cc0a3b8c7128ba92daf2fd700bad7c958fdb7f78
john-bomann <johnabomann@gmail.com>
2022-06-02 16:56:17 -0700

    Set up jest in server

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // In regex form, the i at the end means case insensitive
+  const linkElement = screen.getByText(/protein mutations/i);
   expect(linkElement).toBeInTheDocument();
 });
