f7658b18c7e19a1eec45399a9dcd3b5c1a73f9aa
John Bomann <johnabomann@gmail.com>
2022-09-29 15:07:07 -0700

    Added mui, started on dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./Dashboard.js";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box.js";
+// import Toolbar from "@mui/material/Toolbar.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <div style={{ borderBottom: "1px solid black" }}>
+        <h1>Protein Mutations</h1>
+      </div>
+      <Dashboard />
     </div>
   );
 }
