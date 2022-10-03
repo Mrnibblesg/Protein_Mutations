225811344a2b4880855081c785a0e3ac735ba3a1
john-bomann <johnabomann@gmail.com>
2022-10-03 12:11:43 -0700

    Added DB name to mongoose string

--- server/index.js
+++ server/index.js
@@ -1,25 +1,22 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import express from "express";
-import singleprotein from "./routes/singleprotein.js"
+import singleprotein from "./routes/singleprotein.js";
 const app = express();
 const port = 8080;
 
-app.use(express.json())
-app.use("/api",singleprotein)
+app.use(express.json());
+app.use("/api", singleprotein);
 
-mongoose.connect(
-  `mongodb+srv://AdminDB:csci492@cluster0.wj1zjhz.mongodb.net/?retryWrites=true&w=majority`, 
-  {
-  }
-
-).then(() => {
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+mongoose
+  .connect(
+    `mongodb+srv://AdminDB:csci492@cluster0.wj1zjhz.mongodb.net/ProteinMutations?retryWrites=true&w=majority`,
+    {}
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
   });
-}).catch((error) => {
-  console.error(error)
-})
-
-
-
-
--- server/routes/singleprotein.js
+++ server/routes/singleprotein.js
@@ -1,15 +1,18 @@
-import express from "express"
-import Protein from "../models/protein.js"
+import express from "express";
+import Protein from "../models/protein.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/protein-names",(req,res) => {
-    Protein.find({},"name").then((proteins) => {
-        res.status(200).send(proteins)
-    }).catch((error) => {
-        console.error(error)
-        res.status(500).send("Could not retrieve protein names!")
+router.get("/protein-names", (req, res) => {
+  Protein.find({}, "name")
+    .then((proteins) => {
+      console.log(proteins);
+      res.status(200).send(proteins);
     })
-}) 
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Could not retrieve protein names!");
+    });
+});
 
-export default router
+export default router;
